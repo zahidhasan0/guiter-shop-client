6bfa2b616067cd8c9827cd1d7aaa5859d0190b23
zahidhasan0 <zahid.business2002@gmail.com>
2022-11-26 10:52:42 +0600

    allseller

--- src/Pages/Dashboard/AllSeller/AllSeller.js
+++ src/Pages/Dashboard/AllSeller/AllSeller.js
@@ -1,9 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const AllSeller = () => {
+  const { data: allSeller = [] } = useQuery({
+    queryKey: ["allseller"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/allseller");
+      const data = res.json();
+      return data;
+    },
+  });
+  console.log(allSeller);
   return (
-    <div>
-      <h4>This is ALl Sellers route</h4>
+    <div className="overflow-x-auto">
+      <h2 className="text-2xl font-semibold text-center mb-4 text-primary border-b-4 border-primary pb-2 w-1/6">
+        All Seller
+      </h2>
+      <table className="table table-zebra w-full">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Job</th>
+            <th>Favorite Color</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>1</th>
+            <td>Cy Ganderton</td>
+            <td>Quality Control Specialist</td>
+            <td>Blue</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
